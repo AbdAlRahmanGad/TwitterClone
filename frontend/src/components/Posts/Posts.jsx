d4729ad6ffaed2f8bf0573472e3807be392f14d1
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-04-18 02:39:23 +0200

    feat: add Posts component

---
+++ frontend/src/components/Posts/Posts.jsx
@@ -0,0 +1,33 @@
+import style from "./Posts.module.css";
+export function Posts() {
+  return (
+    <>
+    <Post />
+    <Post />
+    <Post />
+    <Post />
+    <Post />
+    </>
+  );
+}
+function Post(){
+  return (
+    <div className={style.post}>
+    <div className={style.userInfo}>
+      <img src="/src/assets/img-1.jpg" alt="" className="profilePicture" />
+      <span className="username">Ahmed Kashkoush</span>
+      <span className="date">April 6, 2024</span>
+    </div>
+
+    <p className={style.content}>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
+      corporis nam provident? Commodi beatae, corporis dolor minus rem dolore
+      minima. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Inventore corporis nam provident? Commodi beatae, corporis dolor minus
+      rem dolore minima. Lorem ipsum dolor sit amet, consectetur adipisicing
+      elit. Inventore corporis nam provident? Commodi beatae, corporis dolor
+      minus rem dolore minima.
+    </p>
+  </div>
+  );
+}
