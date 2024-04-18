e53ba12ce6d134e514f177f2a154dc6c8fb35c57
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-04-18 03:18:04 +0200

    feat: add initial code for suggestions page

--- frontend/src/components/SuggestionsComponent/Suggestions.jsx
+++ frontend/src/components/SuggestionsComponent/Suggestions.jsx
@@ -1,5 +1,40 @@
-export default function Suggestions(){
-    return (
-        <h1 style={{color:"white"}}>Suggestions</h1>
-    )
+import style from "./Suggestions.module.css";
+export default function Suggestions() {
+  return (
+    <div className={style.suggestions}>
+       {/* Search Bar */}
+      <Search />
+       {/* Some Peapole to follow */}
+      <ul className={style.personList}>
+        <Person />
+        <Person />
+        <Person />
+        <Person />
+      </ul>
+      <ul className={style.personList}>
+        <Person />
+        <Person />
+        <Person />
+        <Person />
+      </ul>
+    </div>
+  );
+}
+function Person() {
+  return (
+    <li className={style.person}>
+      <div>
+        <img src="/src/assets/img-1.jpg" alt="" />
+        <div>
+
+        <p className={style.username}>AK</p>
+        <p className={style.subTitle}>@AK</p>
+        </div>
+      </div>
+      <button className={style.btn}>follow</button>
+    </li>
+  );
+}
+function Search() {
+  return <input className={style.search} type="search" placeholder="Search..." />;
 }
