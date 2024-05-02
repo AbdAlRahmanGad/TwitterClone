a485e2e9d3f56a5a2b9f4fa37a6908820169766d
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-05-02 16:35:12 +0300

    chore: include profile content into profile Page

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,13 +1,23 @@
 /* eslint-disable react/prop-types */
 import PageNav from "../components/PageNavComponent/PageNav";
-import Profile from "../components/ProfileComponent/Profile";
 import Suggestions from "../components/SuggestionsComponent/Suggestions";
+import { ProfileHeader } from "../components/ProfileComponent/ProfileHeader";
+import { ProfileHero } from "../components/ProfileComponent/ProfileHero";
+import  ProfileNav  from "../components/ProfileComponent/ProfileNav/ProfileNav";
+import { Outlet } from "react-router-dom";
 
-export default function ProfilePage({ showReplies }) {
+import style from "./ProfilePage.module.css";
+
+export default function ProfilePage() {
   return (
     <main className="main">
       <PageNav />
-      <Profile showReplies={showReplies} />
+      <section className={`${style.profile} border-rl`}>
+        <ProfileHeader />
+        <ProfileHero />
+        <ProfileNav />
+        <Outlet />
+      </section>
       <Suggestions />
     </main>
   );
