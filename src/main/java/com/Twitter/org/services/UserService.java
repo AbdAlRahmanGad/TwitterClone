25429a944b1f191e9cce9b6f3d673770253de66c
AbdAlRahmanGad <abdobngad@gmail.com>
2024-05-06 02:01:30 +0300

    Implemented add, delete, get User

--- src/main/java/com/Twitter/org/services/UserService.java
+++ src/main/java/com/Twitter/org/services/UserService.java
@@ -5,5 +5,7 @@ import com.Twitter.org.Models.Users.User;
 public interface UserService {
 
     User findUserByUsername(String username);
+    public User save(User user);
 
+    void delete(String username);
 }
