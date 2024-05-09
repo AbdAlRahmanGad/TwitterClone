2ddb234bd6ae6fff853a1e5d79619128976000ba
Ahmed Mohamed <am0103738@gmail.com>
2024-05-09 21:29:17 +0300

    Implemented reposts counter in StartupDataInitializer, Deleted blockedBy table

--- src/main/java/com/Twitter/org/initializer/StartupDataInitializer.java
+++ src/main/java/com/Twitter/org/initializer/StartupDataInitializer.java
@@ -2,6 +2,7 @@ package com.Twitter.org.initializer;
 
 import com.Twitter.org.services.Impl.BookmarksServiceImpl;
 import com.Twitter.org.services.Impl.LikesServiceImpl;
+import com.Twitter.org.services.Impl.RepostsServiceImpl;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -10,17 +11,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class StartupDataInitializer {
 
-    @Autowired
-    private LikesServiceImpl likesService;
+    private final LikesServiceImpl likesService;
+    private final BookmarksServiceImpl bookmarksService;
+    private final RepostsServiceImpl repostsService;
 
     @Autowired
-    private BookmarksServiceImpl bookmarksService;
+    public StartupDataInitializer(LikesServiceImpl likesService, BookmarksServiceImpl bookmarksService, RepostsServiceImpl repostsService) {
+        this.likesService = likesService;
+        this.bookmarksService = bookmarksService;
+        this.repostsService = repostsService;
+    }
 
-    // TODO: ADD Missing Counters here
+    // TODO: ADD Missing Counters for the Tweets: Comments Counter
 
     @PostConstruct
     public void initializeData() {
         likesService.updateLikesCountersOnStartup();
         bookmarksService.updateBookmarksCountersOnStartup();
+        repostsService.updateRepostsCountersOnStartup();
     }
 }
--- src/main/java/com/Twitter/org/services/Impl/RepostsServiceImpl.java
+++ src/main/java/com/Twitter/org/services/Impl/RepostsServiceImpl.java
@@ -1,11 +1,12 @@
 package com.Twitter.org.services.Impl;
 
-import java.util.List;
-
 import com.Twitter.org.Models.Tweets.Tweets;
 import com.Twitter.org.Repository.TweetsRepository;
 import com.Twitter.org.services.RepostsService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class RepostsServiceImpl implements RepostsService {
@@ -30,7 +31,7 @@ public class RepostsServiceImpl implements RepostsService {
     public Tweets repostTweet(String username, int tweetId) {
         Tweets originalTweet = tweetsRepository.findById(tweetId).get();
         originalTweet.setRepostNumber(originalTweet.getRepostNumber() + 1);
-      return tweetsRepository.save(new Tweets().
+        return tweetsRepository.save(new Tweets().
                 builder()
                 .authorId(username)
                 .parentId(tweetId)
@@ -56,7 +57,7 @@ public class RepostsServiceImpl implements RepostsService {
 
     @Override
     public void deleteRepost(String username, int tweetId) {
-        Tweets tweet =  tweetsRepository.findById(tweetId).get();
+        Tweets tweet = tweetsRepository.findById(tweetId).get();
         Tweets originalTweet = tweetsRepository.findById(tweet.getOriginalPost()).get();
         originalTweet.setRepostNumber(originalTweet.getRepostNumber() - 1);
         tweetsRepository.save(originalTweet);
@@ -65,6 +66,20 @@ public class RepostsServiceImpl implements RepostsService {
 
     @Override
     public boolean hasUserRepostedTweet(String username, int tweetId) {
-        return  tweetsRepository.hasUserRepostedTweet(tweetId, username) > 0;
+        return tweetsRepository.hasUserRepostedTweet(tweetId, username) > 0;
+    }
+
+    // This method updates the reposts counter for all tweets on startup
+    // If you inserted some reposts manually in the database
+    @Transactional
+    public void updateRepostsCountersOnStartup() {
+        // Get all tweets
+        List<Tweets> allTweets = (List<Tweets>) tweetsRepository.findAll();
+
+        // For each tweet, count the number of reposts and update the reposts counter
+        for (Tweets tweet : allTweets) {
+            int repostsCount = tweetsRepository.countReposts(tweet.getId());
+            tweet.setRepostNumber(repostsCount);
+        }
     }
 }
